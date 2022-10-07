0d2c36b4291fdedf590eaac5f3c4b8c4e4dbacba
s459315 <s459315@st.amu.edu.pl>
2022-10-08 00:42:43 +0200

    Added BMI Calculator page

---
+++ NutriApp/Controllers/BMIController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NutriApp.Models;
+
+namespace NutriApp.Controllers
+{
+    public class BMIController : Controller
+    {
+        public IActionResult Index(int? weight, int? height)
+        {
+            if (weight.HasValue && height.HasValue)
+            {
+                BMIEntry entry = new BMIEntry(weight.Value, height.Value);
+                var r = entry.BMI;
+                return View(entry);
+            }
+            return View();
+        }
+
+        public static string GetWeightCategory(double bmi)
+        {
+            switch (bmi)
+            {
+                case < 16:
+                    return "Severe Thinness";
+                case <= 17:
+                    return "Moderate Thinness";
+                case <= 18.5:
+                    return "Mild Thinness";
+                case <= 25:
+                    return "Normal";
+                case <= 30:
+                    return "Overweight";
+                case <= 35:
+                    return "Obese Class I";
+                case <= 40:
+                    return "Obese Class II";
+                case > 40:
+                    return "Obese Class III";
+            }
+            return "";
+        }
+
+        public static double GetLowRangeForHeight(int height)
+        {
+            double _height = Convert.ToDouble(height);
+            return Math.Round(18.5 * Math.Pow(_height/100, 2));
+        }
+
+        public static double GetHighRangeForHeight(int height)
+        {
+            double _height = Convert.ToDouble(height);
+            return Math.Round(25 * Math.Pow(_height/100, 2));
+        }
+
+        public static double HowMuchToLose(BMIEntry entry)
+        {
+            var toLose = entry.BMI - 25;
+            var power = Math.Pow(Convert.ToDouble(entry.Height) / 100, 2);
+            var result = toLose * power;
+
+            return Math.Round(result, 2);
+        }
+
+        public static double HowMuchToGain(BMIEntry entry)
+        {
+            var toLose = 18.5 - entry.BMI;
+            var power = Math.Pow(Convert.ToDouble(entry.Height) / 100, 2);
+            var result = toLose * power;
+
+            return Math.Round(result, 2);
+        }
+    }
+}
---
+++ NutriApp/Models/BMIEntry.cs
@@ -0,0 +1,23 @@
+﻿namespace NutriApp.Models
+{
+    public class BMIEntry
+    {
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public BMIEntry(int weight, int height)
+        {
+            Weight = weight;
+            Height = height;
+        }
+
+        public double BMI
+        {
+            get {
+                double v = Convert.ToDouble(Weight);
+                double h = Convert.ToDouble(Height);
+                return Math.Round(v / Math.Pow(h / 100.0, 2), 2); 
+            }
+        }
+    }
+
+}
